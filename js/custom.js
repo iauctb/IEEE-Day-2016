e43742f5b15b54a3b53552fb142eb4f63e6b6afe
Javid Izadfar <javid.izadfar@gmail.com>
2016-11-20 21:43:54 +0330

    Added Soft Scroll

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,29 @@
 // Typed.js
 $(function(){
-      $("#intro .typed span").typed({
-         strings: ["مهندسان برق", "مهندسان الکترونیک", "مهندسان کامپیوتر", "مهندسان مخابرات", "دانشجویان برق", "دانشجویان الکترونیک", "دانشجویان کامپیوتر", "دانشجویان مخابرات", "اساتید دانشگاه", "همه‌", "همه‌ی دانشجویان", "دانشگاه آزاد تهران مرکزی"],
-         typeSpeed: 80,
-         backDelay: 1000,
-         shuffle: true,
-         loop: true,
-      });
+   $("#intro .typed span").typed({
+      strings: ["مهندسان برق", "مهندسان الکترونیک", "مهندسان کامپیوتر", "مهندسان مخابرات", "دانشجویان برق", "دانشجویان الکترونیک", "دانشجویان کامپیوتر", "دانشجویان مخابرات", "اساتید دانشگاه", "همه‌", "همه‌ی دانشجویان", "دانشگاه آزاد تهران مرکزی"],
+      typeSpeed: 80,
+      backDelay: 1000,
+      shuffle: true,
+      loop: true,
    });
+});
+
+// Soft Scroll
+$(function(){
+   var pattern = /^(#)\w+/;
+
+   $('a').each(function(){
+      $anchor = $(this).attr('href');
+      if ( pattern.test($anchor) ) {
+         $(this).on('click', function(event){
+            console.log($($anchor));
+            $('html, body').stop().animate({
+               scrollTop: $($anchor).offset().top
+            }, 1000, 'swing');
+            event.preventDefault();
+         })
+      }
+   })
+
+});
