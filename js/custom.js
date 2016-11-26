101f60c2f6fcbc27817a66c423eb8acaa4f04532
Javid Izadfar <javid.izadfar@gmail.com>
2016-11-26 14:55:46 +0330

    Adde Google map

--- js/custom.js
+++ js/custom.js
@@ -53,3 +53,58 @@ atvImg();
 $(function() {
    countdown(year,month,day,hour,minute)
 })
+
+
+// Google Map
+var params;
+window.onload = function () {
+
+    //if (typeof google !== "undefined"){
+    if (window.google && google.maps) {
+        // Map script is already loaded
+        console.log("Map script is already loaded. Initialising...");
+        initializeMap();
+    } else {
+        console.log("Lazy loading Google map...");
+        lazyLoadGoogleMap();
+    }
+
+};
+
+function initialize(params) {
+  var isDraggable = !('ontouchstart' in document.documentElement);
+  var office = new google.maps.LatLng(35.76205957, 51.33975732);
+  var mapOptions = {
+      center: office,
+      zoom: 15,
+      scrollwheel: false,
+      draggable: isDraggable,
+      mapTypeControl: false,
+      streetViewControl: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: office,
+      map: map,
+      title: "دانشگاه آزاد تهران مرکز",
+      icon:'img/pointer.png'
+  });
+  marker.setMap(map);
+}
+
+function lazyLoadGoogleMap() {
+  $.getScript("http://maps.google.com/maps/api/js?language=fa&callback=initializeMap")
+  .done(function (script, textStatus) {
+      console.log("Google map script loaded successfully");
+  })
+  .fail(function (jqxhr, settings, ex) {
+      console.log("Could not load Google Map script: " + jqxhr);
+  });
+}
+
+function initializeMap() {
+  initialize(params);
+}
